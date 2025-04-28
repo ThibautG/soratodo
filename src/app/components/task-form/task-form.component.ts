cc4b9bb1a34aa1189611921d6c4aec14294ed8f3
ThibautG <thibaut.gaudinat@gmail.com>
2025-04-28 15:44:37 +0200

    ajout méthode onSubmit + utilisation de FormsModule pour Angular

--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import {TaskService} from '../../services/task.service';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-task-form',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './task-form.component.html',
   styleUrl: './task-form.component.scss'
 })
 export class TaskFormComponent {
+  // on crée une tâche qui va recevoir ce qu'on entre dans le form
+  newTask = {
+    title: '',
+    description: ''
+  }
 
+
+  // on appelle les services pour utiliser addTask
+  constructor(private taskService: TaskService) {  }
+
+  // on crée une méthode pour ajouter la nouvelle tâche depuis le form
+  onSubmit() {
+    /*console.log(this.newTask)*/
+    //on s'assure qu'au moins le champ title est rempli
+    if (this.newTask.title.trim()) {
+      // on ajoute la nouvelle tâche
+      this.taskService.addTask(this.newTask);
+      // on réinitialise le formulaire
+      this.newTask = {
+        title: '',
+        description: ''
+      };
+    }
+  }
 }
