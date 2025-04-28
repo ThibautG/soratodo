de957b3ba0a24e5967c69ffa26503b21f810bd86
ThibautG <thibaut.gaudinat@gmail.com>
2025-04-28 12:16:10 +0200

    affichage TaskListComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './components/header/header.component';
 import {TaskFormComponent} from './components/task-form/task-form.component';
+import {TaskListComponent} from './components/task-list/task-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, TaskFormComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    TaskFormComponent,
+    TaskListComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
