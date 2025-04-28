722650d600afb60bd64ba492f761d73520c8e411
ThibautG <thibaut.gaudinat@gmail.com>
2025-04-28 16:09:36 +0200

    modif service pour stocker liste dans localStorage

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,6 +1,9 @@
 import {Injectable} from '@angular/core';
 import {Task} from '../models/task';
 
+// Clé pour stocker dans localStorage
+const TASKS_KEY = 'soraToDoTasks';
+
 @Injectable ({
   providedIn: 'root'
 })
@@ -10,18 +13,33 @@ export class TaskService {
   // on crée un tableau vide pour stocker les tâches
   private tasks: Task[] = [];
 
+  // on charge les tâches depuis le localStorage au démarrage du service
+  constructor() {
+    this.loadTasks();
+  }
+  // méthode pour charger les tâches depuis localStorage
+  private loadTasks() {
+    const storedTasks = localStorage.getItem(TASKS_KEY);
+    if (storedTasks) {
+      this.tasks = JSON.parse(storedTasks);
+    }
+    console.log(this.tasks)
+  }
+  // méthode pour sauvegarder les tâches dans le localStorage
+  private saveTasks() {
+    localStorage.setItem(TASKS_KEY, JSON.stringify(this.tasks));
+  }
+
   // méthode pour récupérer la liste de tâches
   getTasks(): Task[] {
     // on retourne une copie du tableau pour travailler plus proprement dessus
     return [...this.tasks];
   }
-
   // méthode pour ajouter une tâche,
-    // on prend bien une Task en entrée, mais on omit l'id et le status,
-    // pas demandés dans le formulaire
+  // on prend bien une Task en entrée, mais on omit l'id et le status,
+  // pas demandés dans le formulaire
   addTask(task: Omit<Task, 'id' | 'status'>): void {
     /*console.log(task)*/
-
     // on ajoute id et status "à faire" à la tâche du formulaire
     // TODO : faire une vérification si id présent dans tasks
     const newTask: Task = {
@@ -29,7 +47,9 @@ export class TaskService {
       id: crypto.randomUUID().substring(0, 8),
       status: "à faire"
     };
-    //on push le tout dans le tableau tasks
+    // on push le tout dans le tableau tasks
     this.tasks.push(newTask);
+    // on sauvegarde la nouvelle liste dans localStorage
+    this.saveTasks();
   }
 }
