456ce5c8e410c2e2cf1bf55cc52c758e3c733197
ThibautG <thibaut.gaudinat@gmail.com>
2025-04-28 17:13:01 +0200

    gestion cas où liste vide

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,12 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {Task} from '../../models/task';
 import {TaskService} from '../../services/task.service';
-import {NgForOf} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-task-list',
   imports: [
-    NgForOf
+    NgForOf,
+    NgIf
   ],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
