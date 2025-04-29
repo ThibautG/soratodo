c6eeb3502575db4fcae8630ea6018c58ed655469
ThibautG <thibaut.gaudinat@gmail.com>
2025-04-29 11:59:55 +0200

    création des routes + utilisation router outler

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import {
+  LandingPageComponent
+} from './components/landing-page/landing-page.component';
+import {TaskListComponent} from './components/task-list/task-list.component';
+import {TaskFormComponent} from './components/task-form/task-form.component';
+import {
+  TaskDetailComponent
+} from './components/task-detail/task-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: LandingPageComponent},
+  {path: 'tasks', component: TaskListComponent},
+  {path: 'tasks/new', component: TaskFormComponent},
+  {path: 'tasks/:id', component: TaskDetailComponent}
+  /* TODO: penser à la route /tasks/:id/edit : modification d’une tâche.*/
+];
