853e5f61e50d5726d01fc986d4417faa83598a52
ThibautG <thibaut.gaudinat@gmail.com>
2025-04-29 15:39:20 +0200

    Affichage conditionnel nouvelle tâche | modifier + modification méthode onSubmit

--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TaskService} from '../../services/task.service';
 import {FormsModule} from '@angular/forms';
 import {TaskStatusType} from '../../models/task-status-type.type';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Task} from '../../models/task';
 
 @Component({
   selector: 'app-task-form',
@@ -12,7 +13,7 @@ import {Router} from '@angular/router';
   templateUrl: './task-form.component.html',
   styleUrl: './task-form.component.scss'
 })
-export class TaskFormComponent {
+export class TaskFormComponent implements OnInit{
   // on crée une tâche qui va recevoir ce qu'on entre dans le form
   newTask: {
     title: string;
@@ -24,28 +25,48 @@ export class TaskFormComponent {
     status: 'à faire'
   };
 
+  // Booléen pour annoncer si mode Edit ou non
+  isEditMode: boolean = false;
+  // string pour stocker l'id si présent dans url
+  taskId: string | null = null;
 
-
-  // on appelle les services pour utiliser addTask
+  // on appelle les services
   constructor(
     private taskService: TaskService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
     ) {  }
 
+  ngOnInit(): void {
+    // stocker l'id présent dans la route
+    this.taskId = this.route.snapshot.params['id'];
+
+    if (this.taskId) {
+      const  existingTask: Task = this.taskService.getTaskById(this.taskId);
+
+      if (existingTask) {
+        this.newTask = {
+          title: existingTask.title,
+          description: existingTask.description,
+          status: existingTask.status
+        };
+        this.isEditMode = true;
+      }
+    }
+  }
+
   // on crée une méthode pour ajouter la nouvelle tâche depuis le form
-  onSubmit() {
+  onSubmit(): void {
     /*console.log(this.newTask)*/
     //on s'assure qu'au moins le champ title est rempli
     if (this.newTask.title.trim()) {
-      // on ajoute la nouvelle tâche
-      this.taskService.addTask(this.newTask);
-      // on réinitialise le formulaire
-      this.newTask = {
-        title: '',
-        description: '',
-        status: 'à faire'
-      };
-
+      if (this.isEditMode && this.taskId) {
+        // si on est en mode edit et qu'on a bien un id dans url
+        this.taskService.updateTask(this.taskId, this.newTask);
+      } else {
+        // sinon, on ajoute la nouvelle tâche
+        this.taskService.addTask(this.newTask);
+      }
       // on navigue vers la route de la liste
       this.router.navigateByUrl('tasks');
     }
