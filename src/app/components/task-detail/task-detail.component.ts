2fd83883ec4761112905193ee5435fa38eb7602e
ThibautG <thibaut.gaudinat@gmail.com>
2025-04-30 10:44:13 +0200

    création component détails + gestion si tâche non existante

--- src/app/components/task-detail/task-detail.component.ts
+++ src/app/components/task-detail/task-detail.component.ts
@@ -1,17 +1,63 @@
-import {Component, Input} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Task} from '../../models/task';
-import {LowerCasePipe} from '@angular/common';
+import {NgIf, TitleCasePipe} from '@angular/common';
+import {TaskService} from '../../services/task.service';
+import {ActivatedRoute, Router, RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-task-detail',
   imports: [
-    LowerCasePipe
+    NgIf,
+    TitleCasePipe,
+    RouterLink
   ],
   templateUrl: './task-detail.component.html',
   styleUrl: './task-detail.component.scss'
 })
-export class TaskDetailComponent {
-  // on reçoit une tâche depuis l'élément parent
-  @Input() task!: Task;
+export class TaskDetailComponent implements OnInit{
+  // string pour stocker l'id si présent dans url
+  taskId: string | null = null;
+  // task pour stocker la tâche correspondante
+  task: Task | undefined;
+  // un booléen pour cas où tâche non existante
+  taskNotFound: boolean = false;
 
+  // on appelle les services
+  constructor(
+    private taskService: TaskService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {  }
+
+  ngOnInit() {
+    // au chargement, on récupère l'id dans url
+    this.taskId = this.route.snapshot.params['id'];
+
+    if (this.taskId) {
+      // ensuite, on récupère la tâche associée
+      this.task = this.taskService.getTaskById(this.taskId)
+    } else {
+      // s'il n'y a pas de tâche correspondante on switch taskNotFound
+      this.taskNotFound = true;
+    }
+  }
+
+  // Supprimer une tâche au click
+  onDeleteTask(taskId: string) {
+    /* TODO: remplacer les confirmations par des pop-up
+    *   idée : une première méthode pour afficher une div qui contient le
+    *   bouton qui lui seulement appelle onDeleteTask */
+    const confirmation = window.confirm('Êtes-vous sûr de vouloir supprimer' +
+      ' cette tâche ?')
+
+    if (confirmation) {
+      this.taskService.removeTask(taskId);
+      this.router.navigateByUrl(`tasks`)    }
+  }
+
+  // Modifier une tâche au click
+  onModifyTask(taskId: string): void {
+    // au click, on navigue vers la route de modif
+    this.router.navigateByUrl(`tasks/${taskId}/edit`)
+  }
 }
